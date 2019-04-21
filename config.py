0e2cfae5b7b29b356d822884cdcdeb22a8b8f0db
Jestemkioskiem <Dlephhi@gmail.com>
2019-04-22 00:27:58 +0200

    add spacing between variables

--- config.py
+++ config.py
@@ -3,6 +3,7 @@
 # Auth Token for the Discord Bot
 # Necessary to change
 AUTH_TOKEN = ""
+
 # Set to False if you're using an User Token instead of a Bot Token
 AUTH_BOOL = True
 
@@ -11,9 +12,11 @@ AUTH_BOOL = True
 # Necessary to change
 SERVER_ID = 123  # INT not STR!
 
+# Ammount of messages that will be scraped. From newest to oldest.
 MESSAGE_LIMIT = 20000
 
 # IP to your mongoDB database server.
 DB_IP = "mongodb://localhost:27017/"
+
 # Name of your mongoDB database.
 DB_NAME = "Discord"
