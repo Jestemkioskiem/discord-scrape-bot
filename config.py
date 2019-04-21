d4d51c60dac7bb06c18a57a59f8b605a65c54887
Jestemkioskiem <Dlephhi@gmail.com>
2019-04-22 00:27:11 +0200

    specify which variables are necessary to change

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 # config.py
 
 # Auth Token for the Discord Bot
+# Necessary to change
 AUTH_TOKEN = ""
 # Set to False if you're using an User Token instead of a Bot Token
 AUTH_BOOL = True
 
 # Target Server's ID.
 # Can be retrieved by right-clicking the server and choosing "Copy ID"
-
+# Necessary to change
 SERVER_ID = 123  # INT not STR!
 
 MESSAGE_LIMIT = 20000
